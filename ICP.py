297de1ceeef23cdfd8bb5771149aac8a42761eca
Ashish Farande <ashish.farande@gmail.com>
2023-04-21 18:34:14 -0700

    init

---
+++ ICP.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from scipy.spatial.transform import Rotation
+from sklearn.neighbors import NearestNeighbors
+import open3d
+from utils import compare_points
+import copy
+
+
+class Viewer:
+    def __init__(self) -> None:
+        self.vis = open3d.visualization.Visualizer()
+        self.vis.create_window()
+        self.pcd = open3d.geometry.PointCloud()
+
+    def __del__(self):
+        self.vis.destroy_window()
+
+    def add(self, source_pcd: np.array, target_pcd: np.array):
+        tmp = np.hstack((source_pcd, target_pcd))
+        pts = open3d.utility.Vector3dVector(tmp.reshape([-1, 3]))
+        self.pcd.points = pts
+        self.vis.add_geometry(self.pcd)
+
+    def update(self, source_pcd: np.array, target_pcd: np.array):
+        tmp = np.hstack((source_pcd, target_pcd))
+        pts = open3d.utility.Vector3dVector(tmp.reshape([-1, 3]))
+        self.pcd.points = pts
+        self.vis.update_geometry(self.pcd)
+        self.vis.poll_events()
+        self.vis.update_renderer()
+        time.sleep(0.1)
+
+
+class ICP:
+    def __init__(self) -> None:
+        self._viewer_handle = Viewer()
+
+    def __del__(self):
+        self._viewer_handle.__del__()
+
+    def __call__(self, source_pcd, target_pcd):
+
+        source_init = copy.deepcopy(source_pcd)
+
+        T = np.eye(4)
+        orig_centroid = np.mean(source_pcd, axis=0)
+        R = np.array(Rotation.random().as_matrix())
+        # t= np.random.rand(3,1)
+        t = np.zeros((3, 1))
+        source_pcd = source_pcd @ R.T + t.T
+
+        self._viewer_handle.add(source_pcd, target_pcd)
+
+        nn = NearestNeighbors(n_neighbors=1, algorithm='kd_tree')
+        centroid_tgt = np.mean(target_pcd, axis=0)
+
+        for i in range(50):
+            nn.fit(target_pcd)
+            indices = nn.kneighbors(source_pcd, return_distance=False)
+            indices = indices.ravel()
+            matched_dst_pts = target_pcd[indices, :]
+
+            centroid_src = np.mean(source_pcd, axis=0)
+
+            P = source_pcd - centroid_src
+            Q = matched_dst_pts - centroid_tgt
+
+            # rotation matrix
+            M = Q.T @ P
+            U, S, Vt = np.linalg.svd(M)
+            R_int = U @ Vt
+            # special reflection case
+            if np.linalg.det(R) < 0:
+                Vt[-1, :] *= -1
+            R_int = U @ Vt
+            t_int = centroid_tgt - R_int@centroid_src
+
+            source_pcd = source_pcd @ R_int.T + t_int.T
+
+            R = R_int@R
+            t = centroid_tgt - R@orig_centroid
+
+            tmp = source_init @ R.T + t
+            self._viewer_handle.update(tmp, target_pcd)
+
+        # Implement your own algorithm here.
+        T[:3, :3] = R
+        T[:3, 3] = t.squeeze()
+        return T
---
+++ utils.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""Visualization utilies."""
+
+
+def show_points(points):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim3d([-2, 2])
+    ax.set_ylim3d([-2, 2])
+    ax.set_zlim3d([0, 4])
+    ax.scatter(points[:, 0], points[:, 2], points[:, 1])
+
+
+def compare_points(points1, points2):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim3d([-2, 2])
+    ax.set_ylim3d([-2, 2])
+    ax.set_zlim3d([0, 4])
+    ax.scatter(points1[:, 0], points1[:, 2], points1[:, 1])
+    ax.scatter(points2[:, 0], points2[:, 2], points2[:, 1])
+
+
+"""Metric and visualization."""
+
+
+def compute_rre(R_est: np.ndarray, R_gt: np.ndarray):
+    """Compute the relative rotation error (geodesic distance of rotation)."""
+    assert R_est.shape == (
+        3, 3), 'R_est: expected shape (3, 3), received shape {}.'.format(R_est.shape)
+    assert R_gt.shape == (
+        3, 3), 'R_gt: expected shape (3, 3), received shape {}.'.format(R_gt.shape)
+    # relative rotation error (RRE)
+    rre = np.arccos(np.clip(0.5 * (np.trace(R_est.T @ R_gt) - 1), -1.0, 1.0))
+    return rre
+
+
+def compute_rte(t_est: np.ndarray, t_gt: np.ndarray):
+    assert t_est.shape == (
+        3,), 't_est: expected shape (3,), received shape {}.'.format(t_est.shape)
+    assert t_gt.shape == (
+        3,), 't_gt: expected shape (3,), received shape {}.'.format(t_gt.shape)
+    # relative translation error (RTE)
+    rte = np.linalg.norm(t_est - t_gt)
+    return rte
