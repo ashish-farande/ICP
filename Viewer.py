4b82e7d00090eec0c89c3808937e7eda9405600d
Ashish Farande <ashish.farande@gmail.com>
2023-04-21 18:56:19 -0700

    protect the viewer data

--- Viewer.py
+++ Viewer.py
@@ -4,25 +4,27 @@ import numpy as np
 
 
 class Viewer:
+    SLEEP_INTERVAL_S: float = 0.1
+
     def __init__(self) -> None:
-        self.vis = open3d.visualization.Visualizer()
-        self.vis.create_window()
-        self.pcd = open3d.geometry.PointCloud()
+        self._vis = open3d.visualization.Visualizer()
+        self._vis.create_window()
+        self._pcd = open3d.geometry.PointCloud()
 
     def __del__(self):
-        self.vis.destroy_window()
+        self._vis.destroy_window()
 
     def add(self, source_pcd: np.array, target_pcd: np.array):
         tmp = np.hstack((source_pcd, target_pcd))
         pts = open3d.utility.Vector3dVector(tmp.reshape([-1, 3]))
-        self.pcd.points = pts
-        self.vis.add_geometry(self.pcd)
+        self._pcd.points = pts
+        self._vis.add_geometry(self._pcd)
 
     def update(self, source_pcd: np.array, target_pcd: np.array):
         tmp = np.hstack((source_pcd, target_pcd))
         pts = open3d.utility.Vector3dVector(tmp.reshape([-1, 3]))
-        self.pcd.points = pts
-        self.vis.update_geometry(self.pcd)
-        self.vis.poll_events()
-        self.vis.update_renderer()
-        time.sleep(0.1)
+        self._pcd.points = pts
+        self._vis.update_geometry(self._pcd)
+        self._vis.poll_events()
+        self._vis.update_renderer()
+        time.sleep(Viewer.SLEEP_INTERVAL_S)
